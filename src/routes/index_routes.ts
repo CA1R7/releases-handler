c64091c123216c3019af29f1f3ec1aeebff2b565
CA1R7 <cairzz222@gmail.com>
2021-09-01 11:57:54 +0100

    Added static route for releases files

--- src/routes/index_routes.ts
+++ src/routes/index_routes.ts
@@ -1,8 +1,12 @@
-import { Router } from "express";
+import { join } from "path";
+import express, { Router } from "express";
 import updatesManager from "./generale/updates_manager";
 
 const router: Router = Router();
 
 router.use("/updates", updatesManager);
+
+// Added static releases folder conatins raw_binarys files.
+router.use("/releases", express.static(join(__dirname, "../../releases")));
 
 export default router;
